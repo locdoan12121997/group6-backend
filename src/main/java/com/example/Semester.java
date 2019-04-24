f99327e7c6256f6e1a7122928e8ffbc526aa036c
Loc Doan <cs2015_loc.dtt@student.vgu.edu.vn>
2019-04-24 15:23:01 +0700

    edit semester

--- src/main/java/com/example/Semester.java
+++ src/main/java/com/example/Semester.java
@@ -27,44 +27,44 @@ public class Semester {
     public JsonObject getSemester() {
         String ans = "";
         try {
-//            Statement stm = Main.connection.createStatement();
-//            String sql = "SELECT * FROM Semester";
-//            ResultSet rs = stm.executeQuery(sql);
-//            JsonBuilderFactory factory = Json.createBuilderFactory(config);
-////            JsonArray value = factory.createArrayBuilder();
-//            String pattern = "yyyy-MM-dd HH:mm:ss";
-//            DateFormat df = new SimpleDateFormat(pattern);
-//            while (rs.next()){
-//                int id = rs.getInt("id");
-//                Date dbSqlDate = rs.getDate("from_time");
-//                String todayAsString = df.format(dbSqlDate);
-////                value.add(factory.createObjectBuilder()
-////                                .add("id", id)
-////                                .add("from_date", todayAsString));
-//            }
-//            rs.close();
-//            stm.close();
-//            JsonObject result = factory.createObjectBuilder()
-//                    .add("data", value);
-            JsonBuilderFactory factory = Json.createBuilderFactory(null);
-            JsonObject value = factory.createObjectBuilder()
-                    .add("firstName", "John")
-                    .add("lastName", "Smith")
-                    .add("age", 25)
-                    .add("address", factory.createObjectBuilder()
-                            .add("streetAddress", "21 2nd Street")
-                            .add("city", "New York")
-                            .add("state", "NY")
-                            .add("postalCode", "10021"))
-                    .add("phoneNumber", factory.createArrayBuilder()
-                            .add(factory.createObjectBuilder()
-                                    .add("type", "home")
-                                    .add("number", "212 555-1234"))
-                            .add(factory.createObjectBuilder()
-                                    .add("type", "fax")
-                                    .add("number", "646 555-4567")))
+            Statement stm = Main.connection.createStatement();
+            String sql = "SELECT * FROM Semester";
+            ResultSet rs = stm.executeQuery(sql);
+            JsonBuilderFactory factory = Json.createBuilderFactory(config);
+//            JsonArray value = factory.createArrayBuilder();
+            String pattern = "yyyy-MM-dd HH:mm:ss";
+            DateFormat df = new SimpleDateFormat(pattern);
+            while (rs.next()){
+                int id = rs.getInt("id");
+                Date dbSqlDate = rs.getDate("from_time");
+                String todayAsString = df.format(dbSqlDate);
+//                value.add(factory.createObjectBuilder()
+//                                .add("id", id)
+//                                .add("from_date", todayAsString));
+            }
+            rs.close();
+            stm.close();
+            JsonObject result = factory.createObjectBuilder()
+                    .add("data", value.build())
+//            JsonBuilderFactory factory = Json.createBuilderFactory(null);
+//            JsonObject value = factory.createObjectBuilder()
+//                    .add("firstName", "John")
+//                    .add("lastName", "Smith")
+//                    .add("age", 25)
+//                    .add("address", factory.createObjectBuilder()
+//                            .add("streetAddress", "21 2nd Street")
+//                            .add("city", "New York")
+//                            .add("state", "NY")
+//                            .add("postalCode", "10021"))
+//                    .add("phoneNumber", factory.createArrayBuilder()
+//                            .add(factory.createObjectBuilder()
+//                                    .add("type", "home")
+//                                    .add("number", "212 555-1234"))
+//                            .add(factory.createObjectBuilder()
+//                                    .add("type", "fax")
+//                                    .add("number", "646 555-4567")))
                     .build();
-            return value.toString();
+            return result.toString();
         }
         catch (Exception exp){
             ans = exp.toString();
