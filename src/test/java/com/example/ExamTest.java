ac4f26080e02fb6da791faaab95ae6e46a4459c8
Loc Doan <cs2015_loc.dtt@student.vgu.edu.vn>
2019-04-30 13:34:08 +0700

    fix exam test

--- src/test/java/com/example/ExamTest.java
+++ src/test/java/com/example/ExamTest.java
@@ -22,49 +22,69 @@ public class ExamTest {
 
     @Test
     public void testCreateExam() throws Exception {
-        Exam.CreateExam("1111-11-20", "08:00:00", "10:00:00", "1111-11-11", 1234);
+        Semester.CreateSemester("1111-11-01", "1111-11-11");
+        int semester_id = Main.LastInsertId();
+        Module.CreateModule("test module", "testing", semester_id);
+        int module_id = Main.LastInsertId();
+        Exam.CreateExam("1111-11-20", "08:00:00", "10:00:00", "1111-11-11", module_id);
         int last_id = Main.LastInsertId();
-        ResultSet actual = Main.getResultSet("SELECT * FROM Exam " +
-                "WHERE id = " + last_id);
+        ResultSet actual = Main.getResultSet("SELECT * FROM Exam WHERE id = " + last_id + ";");
         if (actual.next()) {
-            assertEquals("1111-11-20", actual.getDate("date_of_exam").toString());
-            assertEquals("1111-11-11", actual.getDate("deadline").toString());
-            assertEquals("08:00:00", actual.getTime("from_time").toString());
-            assertEquals("10:00:00", actual.getTime("to_time").toString());
-            assertEquals(1234,actual.getInt("module_id"));
+            assertEquals("1111-11-20", actual.getString("date_of_exam"));
+            assertEquals("1111-11-11", actual.getString("deadline"));
+            assertEquals("08:00:00", actual.getString("from_time"));
+            assertEquals("10:00:00", actual.getString("to_time"));
+            assertEquals(module_id,actual.getInt("module_id"));
         }
-        else fail();
+        else {
+            fail();
+        }
         actual.close();
         Exam.DeleteExam(last_id);
+        Module.DeleteModule(module_id);
+        Semester.DeleteSemester(semester_id);
     }
 
     @Test
     public void testUpdateExam() throws Exception {
-        Exam.CreateExam("1111-11-20", "08:00:00", "10:00:00", "1111-11-11", 1234);
+        Semester.CreateSemester("1111-11-01", "1111-11-11");
+        int semester_id = Main.LastInsertId();
+        Module.CreateModule("test module 1", "testing", semester_id);
+        int module_id = Main.LastInsertId();
+        Exam.CreateExam("1111-11-20", "08:00:00", "10:00:00", "1111-11-11", module_id);
         int last_id = Main.LastInsertId();
         Exam.UpdateExam(last_id, "1111-11-21", "09:00:00", "11:00:00", "1111-11-12");
-        ResultSet actual = Main.getResultSet("SELECT * FROM Exam" +
-                "WHERE id = " +last_id);
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("SELECT * FROM Exam WHERE id = " + last_id + ";");
+        ResultSet actual = Main.getResultSet("SELECT * FROM Exam WHERE id = " + last_id + ";");
         if (actual.next()) {
-            assertEquals("1111-11-21", actual.getDate("date_of_exam").toString());
-            assertEquals("1111-11-12", actual.getDate("deadline").toString());
-            assertEquals("09:00:00", actual.getTime("from_time").toString());
-            assertEquals("11:00:00", actual.getTime("to_time").toString());
-            assertEquals(1234,actual.getInt("module_id"));
+            assertEquals("1111-11-21", actual.getString("date_of_exam"));
+            assertEquals("1111-11-12", actual.getString("deadline"));
+            assertEquals("09:00:00", actual.getString("from_time"));
+            assertEquals("11:00:00", actual.getString("to_time"));
+            assertEquals(module_id,actual.getInt("module_id"));
         }
         else fail();
         actual.close();
         Exam.DeleteExam(last_id);
+        Module.DeleteModule(module_id);
+        Semester.DeleteSemester(semester_id);
     }
 
 
     @Test
     public void testDeleteExam() throws Exception {
-        Exam.CreateExam("1111-11-20", "08:00:00", "10:00:00", "1111-11-11", 1234);
+        Semester.CreateSemester("1111-11-01", "1111-11-11");
+        int semester_id = Main.LastInsertId();
+        Module.CreateModule("test module 2", "testing", semester_id);
+        int module_id = Main.LastInsertId();
+        Exam.CreateExam("1111-11-20", "08:00:00", "10:00:00", "1111-11-11", module_id);
         int last_id = Main.LastInsertId();
         Exam.DeleteExam(last_id);
-        ResultSet actual = Main.getResultSet("SELECT * FROM Semester WHERE id = " + last_id);
+        ResultSet actual = Main.getResultSet("SELECT * FROM Semester WHERE id = " + last_id + ";");
         if (actual.next()) fail();
         actual.close();
+        Module.DeleteModule(module_id);
+        Semester.DeleteSemester(semester_id);
     }
 }
